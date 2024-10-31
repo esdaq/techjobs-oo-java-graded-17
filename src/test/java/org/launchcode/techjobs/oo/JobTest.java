60500ec650198a82683778b6494362f7fb7afea4
d-esda <onedesda@gmail.com>
2024-10-30 23:45:55 -0400

    more progress

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,59 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId(){
+        Job j1 = new Job();
+        Job j2 = new Job();
+        assertNotEquals(j1.getId(), j2.getId(),.001);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job job = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+        assertTrue(job.getName() instanceof String);
+        assertTrue(job.getEmployer() instanceof Employer);
+        assertTrue(job.getLocation() instanceof Location);
+        assertTrue(job.getPositionType() instanceof PositionType);
+        assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals(job.getName(), "Product tester");
+        assertEquals(job.getEmployer().getValue(), "ACME");
+        assertEquals(job.getLocation().getValue(), "Desert");
+        assertEquals(job.getPositionType().getValue(), "Quality control");
+        assertEquals(job.getCoreCompetency().getValue(), "Persistence");
+
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job job1 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+        assertEquals(job1.equals(job2), false);
+    }
+
+    @Test
+    public void testToStringStartsAndEndsWithNewLine() {
+        Job job = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        assertEquals(job.toString(),System.lineSeparator() + "ID:" + job.getId() + System.lineSeparator() +
+                "Name:" + job.getName()+  System.lineSeparator() +
+                "Employer:" + job.getEmployer().getValue() + System.lineSeparator() +
+                "Location:"+ job.getLocation().getValue() + System.lineSeparator() +
+                "Position Type: \n" + System.lineSeparator() +
+                "Core Competency: \n" + System.lineSeparator());
+    }
 }
